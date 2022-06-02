b6adeea04ca9c1ee1fc946db9a7c5c24bc17acf0
Laurence <laurence.freeman@protonmail.com>
2022-06-02 15:57:49 +0100

    refatctor print to logger

--- behave_analysis/utils/mat_to_python.py
+++ behave_analysis/utils/mat_to_python.py
@@ -3,13 +3,15 @@ This script is to help convert that struct away from nasty MatLab into Python
 Input: file path of .mat file which is a struct
 Returns: A python dictionary"""
 
+# OS libaries
 from scipy.io import loadmat
 import scipy
 import mat73
+from loguru import logger
 
 class convert_matlab_struct:
     def __init__(self, file_path_of_mat):
-        print("\nConverting your .mat into a Python Dictionary..")
+        logger.info("Converting your .mat into a Python Dictionary..")
         self.dictionary = self.convert_mat_file(file_path_of_mat)
         
     def convert_mat_file(self, filename):
