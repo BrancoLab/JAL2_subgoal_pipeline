d1eebd7d90388730e7d082291b0a67fee2d96c6a
Laurence <laurence.freeman@protonmail.com>
2022-06-16 12:04:22 +0100

    refactor efizz dic

--- databank.py
+++ databank.py
@@ -11,10 +11,8 @@ databank['session IDs'] = [
                             [1,0, 'NoShelterThenShelter', False, "1677_NoShelterShelter_22MAY31"],
 ]
 
-#need to refactor with philip if possible, the .res file needs a different entry
-# efizz = {"bin": r"D:\Electrophysiology_data\1677_ObstacleThenRemove_22MAY23_g0\1677_ObstacleThenRemove_22MAY23_g0_imec0\1677_ObstacleThenRemove_22MAY23_g0_t0.imec0.ap.bin",
-#          "res": r"D:\Electrophysiology_data\1677_ObstacleThenRemove_22MAY23_g0\1677_ObstacleThenRemove_22MAY23_g0_imec0\1677_ObstacleThenRemove_22MAY23_g0_t0.imec0.ap_res.mat"}
-
-efizz = {"bin": r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap.bin",
-         "res":  r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap_res.mat"}
- 
+efizz = { "1677_NoShelterShelter_22MAY31" : {
+                                              "bin": r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap.bin",
+                                               "res":  r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap_res.mat"
+                                            }
+        }
