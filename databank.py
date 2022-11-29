02f2ed83e4483dc3e2a138834e44162bd5438a11
Laurence <laurence.freeman@live.co.uk>
2022-11-29 14:07:42 +0000

    bump

--- databank.py
+++ databank.py
@@ -1,18 +1,26 @@
 databank = {}
-# databank['path'] = ".\\sample_data\\"                          # sample data
-# databank['path'] = "D:\\Dropbox (UCL)\\DAQ\\upstairs_rig\\"      # full dataset
-# databank['path'] = r"C:\Users\JoannaA\Dropbox (UCL)\DAQ\upstairs_rig"      # full dataset
-# databank['path'] =  r"E:\data\behavior"
-databank['path'] = r"D:\behaviour"
-
+databank['path'] = r"D:\Behaviour"
 databank['session IDs'] = [
-                            #----Name of experiment-----prev session------Folder with data------
-                            [0,0, 'ObstacleThenRemove',   False, "1677_ObstacleThenRemove_22MAY23"],
-                            [1,0, 'NoShelterThenShelter', False, "1677_NoShelterShelter_22MAY31"],
+#----Name of experiment-----prev session------Folder with data------
+[0,0, 'NoShelterShelter', False, '1677_NoShelterShelter_22MAY31']
+
 ]
 
-efizz = { "1677_NoShelterShelter_22MAY31" : {
-                                              "bin": r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap.bin",
-                                               "res":  r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap_res.mat"
-                                            }
-        }
+# efizz = {"1677_NoShelterShelter_22MAY31": 
+#             {
+#             "bin": r"D:\efizz\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap.bin"
+#             }
+#         }
+
+# efizz = { "1677_NoShelterShelter_22MAY31" : {
+#                                               "bin": r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap.bin",
+#                                                "res":  r"D:\Electrophysiology_data\1677_NoShelterThenShelter_22MAY31_g0\1677_NoShelterThenShelter_22MAY31_g0_imec0\1677_NoShelterThenShelter_22MAY31_g0_t0.imec0.ap_res.mat"
+#                                             }
+#         }
+
+
+# databank['session IDs'] = [
+#                             #----Name of experiment-----prev session------Folder with data------
+#                             [0,0, 'ObstacleThenRemove',   False, "1677_ObstacleThenRemove_22MAY23"],
+#                             [1,0, 'NoShelterThenShelter', False, "1677_NoShelterShelter_22MAY31"],
+# ]
