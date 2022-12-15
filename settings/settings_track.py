368fdcf95b14f24cd6c0a66d6bd3e4da0d30bdc6
Laurence <laurence.freeman@live.co.uk>
2022-12-15 15:08:24 +0000

    push message to help changing DLC

--- settings/settings_track.py
+++ settings/settings_track.py
@@ -1,4 +1,5 @@
 # Custom lib
+"""Ensure when changing dlc setting file you include the path to the config.yaml file for DLC"""
 
 from behave_analysis.utils.settings_objects import Settings_track
 
@@ -7,13 +8,13 @@ settings_track = Settings_track(
     redo_processing_step=True,
     min_confidence_in_tracking=0.5,
     max_deviation_from_rest_of_points=100, # in pixels
-    display_tracking_output=False, # show a plot of tracking data from DLC
+    display_tracking_output=True, # show a plot of tracking data from DLC
     by_experiment=False,
     experiments = ['no laser'],
     by_session=True,
-    sessions=[1], 
+    sessions=[0], # If running one session and this is set to 1 will run with no errors but will not process anything
     all_sessions=False,
-    dlc_settings_file='D:\\DLC\\NPX_7-Laurence-2022-05-20\\config.yaml', #Change if using a different DLC model
+    dlc_settings_file= r"D:\DLC\NPX_7-Laurence-2022-05-20\config.yaml", #Change if using a different DLC model, you need the path to include the full yaml such as: r"D:\DLC\NPX_7-Laurence-2022-05-20\config.yaml"
     inverse_fisheye_correction_file = '.\\sample_data\\inverse_fisheye_maps.npy' # remove setting if n/a
     
 )
