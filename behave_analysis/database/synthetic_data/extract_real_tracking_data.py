543b6bc9db04667d873e34b7ce6ee9e5a964616d
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-04-18 18:37:40 +0100

    Head direction synthetic data

---
+++ behave_analysis/database/synthetic_data/extract_real_tracking_data.py
@@ -0,0 +1,96 @@
+from databank import experiments_objects
+from behave_analysis.process.process import Process
+import random
+import dill as pickle
+import os
+import numpy as np
+import polars as pl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+for session_ID in experiments_objects:
+    session = Process(session_ID).load_session()
+    break
+
+file = os.path.join(session.file_path, "fully_processed_tracking_data.pickle")
+with open(file, "rb") as dill_file:
+    tracking = pickle.load(dill_file)
+
+headDirection = tracking["hdir"]  # head direction for each frame
+
+print(headDirection)
+
+fps = 40
+
+# Filter head_directions between -0.1 and 0.1 radians
+indices = np.where((-0.1 <= headDirection) & (headDirection <= 0.1))[0]
+
+# Calculate the time of each frame
+times = indices / fps
+
+# Generate the desired number of spikes
+def generate_spikes(spike_count, times, mean, std_dev):
+    np.random.seed(42)  # For reproducibility, you can remove this line for true randomness
+    valid_indices = np.arange(len(times))
+    chosen_indices = np.random.choice(valid_indices, size=spike_count)
+    gaussian_offsets = np.random.normal(mean, std_dev, size=spike_count)
+    spike_times = times[chosen_indices] + gaussian_offsets
+    spike_times.sort()
+    return spike_times
+
+# Example usage
+spike_count = 100000  # The desired number of spikes
+mean = 0  # Mean offset for the Gaussian distribution
+std_dev = 0.1  # Standard deviation for the Gaussian distribution
+spike_times = generate_spikes(spike_count, times, mean, std_dev)
+
+# clusters = np.random.randint(1, 11, size=len(spike_times))
+
+# Create a polar dataframe
+polar_df_hdir = pl.DataFrame(
+    {
+        #  "spike_clusters": clusters.astype(np.int64),
+        "spike_times": [0] * len(spike_times),
+        "spike_clusters": [3] * len(spike_times),
+        "cluster_group": ["good"] * len(spike_times),
+        "aligned_spike_times": spike_times,
+    }
+)
+
+# # Display the polar dataframe
+print(polar_df_hdir)
+
+if __name__ == "__main__":
+    
+    # Plot the head direction tuning curve and the neuron firing polar plot
+    import matplotlib.patches as mpatches
+
+    # Create a proxy artist for the legend
+    blue_patch = mpatches.Patch(color="dodgerblue", alpha=0.75, label="Firing Rate")
+
+    # Convert spike times to indices
+    spike_indices = np.round(spike_times * fps).astype(int)
+
+    # Retrieve head directions corresponding to spike indices
+    spike_head_directions = headDirection[spike_indices]
+
+    # Generate combined plot
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+
+    # Head Direction Tuning Curve
+    sns.histplot(spike_head_directions, bins=100, kde=True, ax=ax1)
+    ax1.set_xlabel("Head Direction (Radians)")
+    ax1.set_ylabel("Spike Count")
+    ax1.set_title("Head Direction Tuning Curve")
+
+    # Neuron Firing Polar Plot
+    ax2 = plt.subplot(1, 2, 2, polar=True)
+    hist, _, _ = ax2.hist(spike_head_directions, bins=36, alpha=0.75, color="dodgerblue")
+    ax2.set_theta_zero_location("N")
+    ax2.set_theta_direction(-1)
+    ax2.set_xticks(np.linspace(0, 2 * np.pi, 8, endpoint=False))
+    ax2.set_xticklabels(["0°", "45°", "90°", "135°", "180°", "225°", "270°", "315°"])
+    ax2.set_title("Neuron Firing Polar Plot")
+    ax2.legend(handles=[blue_patch], loc="upper left")
+
+    plt.show()
--- behave_analysis/database/synthetic_data/generate_data.py
+++ behave_analysis/database/synthetic_data/generate_data.py
@@ -1,26 +1,33 @@
+from behave_analysis.database.synthetic_data.synthetic_data_functions import ImhomogeneousProcess
+from behave_analysis.database.synthetic_data.extract_real_tracking_data import (
+    polar_df_hdir,
+)  # This is the polar dataframe with the spikes fpr the head direction just concat it
+
 from dataclasses import dataclass
 import polars as pl
 import random
-from behave_analysis.database.synthetic_data.synthetic_data_functions import ImhomogeneousProcess
 import pickle as pkl
+
 
 @dataclass(frozen=True)
 class FakeData:
     """A class to store experiment by experiment information"""
-    on_sets: list # Stimulus onset samples
-    dataFrame: pl.DataFrame # Dataframe of the data
-    
+
+    on_sets: list  # Stimulus onset samples
+    dataFrame: pl.DataFrame  # Dataframe of the data
+
+
 class GenerateFakeData:
     def __init__(self):
         self.length_of_session = 225336000
         self.sampling_rate = 30000
-        self.onsets = self.generate_onsets(number_of_onsets = 5)
+        self.onsets = self.generate_onsets(number_of_onsets=5)
         self.spikes = self.generate_fake_spikes()
-        self.clusters = self.generate_fake_clusters(number_of_spikes = len(self.spikes), number_of_clusters = 10)
+        self.clusters = self.generate_fake_clusters(number_of_spikes=len(self.spikes), number_of_clusters=10)
         self.dataframe = self.produce_polars_dataframe()
-        
+
     def generate_onsets(self, number_of_onsets) -> list:
-        """How many onsets do you want to generate? In essence how many 
+        """How many onsets do you want to generate? In essence how many
         trials do you want to simulate?
 
         Args:
@@ -32,35 +39,35 @@ class GenerateFakeData:
         Returns:
             list: _description_
         """
-        
+
         # Check if the number of onsets requested is within a valid range
         if number_of_onsets < 1 or number_of_onsets > self.length_of_session:
             raise ValueError("Count must be between 1 and {}".format(self.length_of_session))
-    
+
         # Use the random.sample function to generate unique random integers
         return random.sample(range(self.length_of_session + 1), number_of_onsets)
 
     def poisson_process(self, onset_time):
-        
         # Params for the kernel
-        T = 10 
+        T = 10
         width_of_kernel = 0.5
         peak_intensity_of_kernel = 50
         num_bins = T
         bin_duration = T / num_bins
-        
-        object = ImhomogeneousProcess(time_end = onset_time + 10, 
-                                      peak_time = onset_time, 
-                                      width = width_of_kernel, 
-                                      peak_intensity = peak_intensity_of_kernel,
-                                      kernel = "gaussian")
-        
-        spike_times = object.events # Tunned to the kernel function
-        
+
+        object = ImhomogeneousProcess(
+            time_end=onset_time + 10,
+            peak_time=onset_time,
+            width=width_of_kernel,
+            peak_intensity=peak_intensity_of_kernel,
+            kernel="gaussian",
+        )
+
+        spike_times = object.events  # Tunned to the kernel function
+
         return spike_times
 
     def generate_fake_spikes(self) -> list:
-        
         # Create a list of lists
         spikes = []
         for idx, onset in enumerate(self.onsets):
@@ -68,16 +75,15 @@ class GenerateFakeData:
             print("Generate trial {} of {}".format(idx + 1, len(self.onsets)))
 
         flat_list = [item for sublist in spikes for item in sublist]
-        
+
         return flat_list
-    
+
     def generate_fake_clusters(self, number_of_spikes, number_of_clusters):
         """Create a fake cluster of spikes all cluster 0"""
         # return [0] * number_of_spikes
         return [random.randint(0, number_of_clusters) for _ in range(number_of_spikes)]
 
     def produce_polars_dataframe(self):
-        
         # Create a Polars DataFrame
         df = pl.DataFrame(
             {
@@ -90,21 +96,25 @@ class GenerateFakeData:
         # Print the DataFrame
         return df
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     path = r"C:\Users\laurence\Documents\JAL-pipeline\behave_analysis\database\synthetic_data\synthetic_dataframe.csv"
     generator = GenerateFakeData()
     dataframe = generator.dataframe
     onsets = generator.onsets
     
-    with open(r"C:\Users\laurence\Documents\JAL-pipeline\behave_analysis\database\synthetic_data\synthetic_onsets.pkl", "wb") as f:
+    print(dataframe)
+
+    with open(
+        r"C:\Users\laurence\Documents\JAL-pipeline\behave_analysis\database\synthetic_data\synthetic_onsets.pkl", "wb"
+    ) as f:
         pkl.dump(onsets, f)
-    
+
+    # Concat angle data
+    # dataframe = dataframe.vstack(polar_df_hdir)
+    dataframe = polar_df_hdir # TODO just look at the head direction
+
     print(dataframe)
     dataframe.write_csv(path)
     
-
-        
-
-
-
+    
