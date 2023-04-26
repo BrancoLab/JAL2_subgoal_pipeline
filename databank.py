611843a4d5ba09b96257440df6ac1f91cdc5ea87
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-04-26 17:17:56 +0100

    adding mushy 5

--- databank.py
+++ databank.py
@@ -1,8 +1,8 @@
 '''A database of all the experiments and mice run in the JJAL team on the big rig'''
 # from behave_analysis.database.experiments import seq4, seq3, seq1, test, laserTest
-from behave_analysis.database.Experiments.JAL001_ex import seq3
+from behave_analysis.database.Experiments.JAL001_ex import seq3, mush5
 from behave_analysis.database.Experiments.JAL002_ex import firstConnection
 from behave_analysis.database.Experiments.testymctestface_ex import testbonsaipulse, testbonsaipulse2withefizz, test_NEWgate
 
 # Place all experiments in a list and 
-experiments_objects = [seq3]
+experiments_objects = [mush5]
