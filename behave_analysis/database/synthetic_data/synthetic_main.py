a2b9af47144542e379306c524bdc30477ef0eb08
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-04-27 15:21:27 +0100

    Bump synth

--- behave_analysis/database/synthetic_data/synthetic_main.py
+++ behave_analysis/database/synthetic_data/synthetic_main.py
@@ -1,8 +1,8 @@
 # Created dataframe has unused columns but required due to indexing in visualize efizz.
 # TODO: remove unused columns from dataframe when indexing is fixed in efizz to be polars 
 
-from behave_analysis.database.synthetic_data.synthetic_data_functions import ImhomogeneousProcess
-from behave_analysis.database.synthetic_data.extract_real_tracking_data import polar_df_hdir # polar_df_sheldir  # This is the polar dataframe with the spikes fpr the head direction just concat it
+from behave_analysis.database.synthetic_data.generate_vectorial_synthetic_neurons import generate_synth_vectorial_cells
+from behave_analysis.database.synthetic_data.poison_process_functions import ImhomogeneousProcess
 from databank import experiments_objects
 from behave_analysis.process.process import Process
 from dataclasses import dataclass
@@ -16,13 +16,7 @@ for session_ID in experiments_objects:
     session = Process(session_ID).load_session()
     break
 
-@dataclass(frozen=True)
-class FakeData:
-    """A class to store experiment by experiment information"""
-    on_sets: list  # Stimulus onset samples
-    dataFrame: pl.DataFrame  # Dataframe of the data
-
-class GenerateFakeData:
+class GenerateFakeDataForRasters:
     def __init__(self):
         self.length_of_session = len(session.ttl.bonsai_TTL)
         self.sampling_rate = 30000
@@ -103,23 +97,23 @@ class GenerateFakeData:
         return df
 
 if __name__ == "__main__":
+    
+    # TODO - Enter where you want your synthetic data to be saved
     path = r"C:\Users\laurence\Documents\JAL-pipeline\behave_analysis\database\synthetic_data\synthetic_dataframe.csv"
-    generator = GenerateFakeData()
-    dataframe = generator.dataframe
-    onsets = generator.onsets
-
-    # Generate fake onsets for the raster plot
-    with open(r"C:\Users\laurence\Documents\JAL-pipeline\behave_analysis\database\synthetic_data\synthetic_onsets.pkl", "wb") as f:
-        pkl.dump(onsets, f)
-
-    # Concat angle data
-    # dataframe = dataframe.vstack(polar_df_hdir)
-    dataframe = polar_df_hdir
-    # dataframe = dataframe.vstack(polar_df_sheldir)
-    # dataframe = polar_df_sheldir
-    # dataframe = polar_df_hdir # TODO just look at the head direction
-
-    print(dataframe)
-    dataframe.write_csv(path)
     
+    # Generate vectorial cells 
+    head_direction_spikes, head_direction_clusters = generate_synth_vectorial_cells(cell_type = "Head_Direction", add_noise = False, number_of_spikes_per_cluster = 100000)
+    shelter_direction_spikes, shelter_direction_clusters = generate_synth_vectorial_cells(cell_type = "Shelter_Direction", add_noise = False, number_of_spikes_per_cluster = 100000)
+
+    # Create a Polars DataFrame - TODO - Comment out if you want to use the vectorial cells
+    head_direction_only = pl.DataFrame({"spike_times": [0] * len(head_direction_spikes), # Not used
+                                        "spike_clusters": head_direction_clusters,
+                                        "cluster_group": ["good"] * len(head_direction_spikes),
+                                         "aligned_spike_times": head_direction_spikes})
+    head_direction_only.write_csv(path)
     
+    # shelter_direction_only = pl.DataFrame({"spike_times": [0] * len(shelter_direction_spikes), # Not used
+    #                                        "spike_clusters": shelter_direction_clusters,
+    #                                        "cluster_group": ["good"] * len(shelter_direction_spikes),
+    #                                        "aligned_spike_times": shelter_direction_spikes})
+    # shelter_direction_only.write_csv(path)
