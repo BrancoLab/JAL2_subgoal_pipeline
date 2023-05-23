e120c52c6b2c863c458c92ea05b4ac5bbd94ba86
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-05-23 19:38:49 +0100

    adding experiment seq1_2

--- behave_analysis/database/Experiments/JAL002_ex.py
+++ behave_analysis/database/Experiments/JAL002_ex.py
@@ -85,3 +85,19 @@ seq1_3_002 = Experiment(# Mouse specific
                     shelter_time = [0, -1],
                     barrier_time = [30, -1],
                     experiment_path = Path(r"002_Sequence1_3_2023_04_28T08_31_30"))
+
+seq1_2_002 = Experiment(# Mouse specific
+                        nick_name = JAL002.nick_name,
+                        total_sessions = JAL002.total_sessions,
+                        mouse_number_pyrat = JAL002.mouse_number_pyrat,
+                        experiment_file_names = JAL002.experiment_file_names,
+                        root_path = JAL002.root_path,
+                        
+                         # Experiment specific
+                    experiment_name = 'sequence1_2',
+                    experiment_idx = 0,
+                    experiment_date = "2023_04_25",
+                    experiment_time = "07_32_30",
+                    shelter_time = [0, -1],
+                    barrier_time = [125, -1], # assuming 125 minutes is 125
+                    experiment_path = Path(r"002_seq1_2_2023_04_25T07_32_30"))
