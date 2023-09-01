96483c19ba81b9e33540163b4c4fe4395023bfeb
JRegg66 <jasmine.reggiani@gmail.com>
2023-09-01 09:06:21 +0100

    JAL3 added

---
+++ behave_analysis/database/Experiments/JAL003_ex.py
@@ -0,0 +1,20 @@
+from behave_analysis.database.Experiments.experiment_class import Experiment
+from behave_analysis.database.Mice.JAL003 import JAL003
+from pathlib import Path
+
+JAL3_flip_rot = Experiment(# Mouse specific
+                    nick_name = JAL003.nick_name,
+                    total_sessions = JAL003.total_sessions,
+                    mouse_number_pyrat = JAL003.mouse_number_pyrat,
+                    experiment_file_names = JAL003.experiment_file_names,
+                    root_path = JAL003.root_path,
+
+                    # Experiment specific
+                    experiment_name = 'mushroom',
+                    experiment_idx = 0,
+                    experiment_date = "2023_08_25",
+                    experiment_time = "09_42_06",
+                    shelter_time = [0, -1],
+                    barrier_time = [58, 184],
+                    barrief_flip_time = [184, -1],
+                    experiment_path = Path(r"003_flip_rotated_2023_08_25T09_42_06"))
---
+++ behave_analysis/database/Mice/JAL003.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from behave_analysis.database.Mice.mouse_class import Mouse
+
+# Mices
+JAL003 = Mouse(
+    nick_name="JAL003",
+    total_sessions=3,
+    mouse_number_pyrat="BAA-1103439",
+    experiment_file_names=None,
+    # root_path=Path(r"D:\efizz\MouseID_001"),
+    root_path = Path(r"E:\Experimental_Data\JAL003")
+)
