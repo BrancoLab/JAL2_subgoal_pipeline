682b09783299035549f47e3ae2de6de16295764f
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-09-03 11:10:15 +0100

    Adding JAL003 mouse

---
+++ behave_analysis/database/Mice/JAL003.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from behave_analysis.database.Mice.mouse_class import Mouse
+
+# Mices
+JAL002 = Mouse(
+    nick_name="JAL002",
+    total_sessions=0,
+    mouse_number_pyrat="xxx-xxxxxxx",
+    experiment_file_names=None,
+    root_path=Path(r"E:\efizz\JAL003"),
+    # root_path=Path(r"E:\Experimental_Data\JAL002")
+)
