798a4f51fb745d6c499749dc24a8b0e268fb6455
LaurenceFreeman <laurence.1.freeman@gmail.com>
2023-10-09 16:58:46 +0100

    Generate marginal vs optimal plots, behave

---
+++ behave_analysis/visualize/behaviour/marginals.py
@@ -0,0 +1,217 @@
+"""
+
+The point of this script to observe how well we have sampled all of the angles of interest. It does this by plotting all of the marginals as probability distribution functions (PDFs) and comparing them to the optimal marginals. 
+The optimal marginals are the marginals that would be observed if the mouse was sampling all of the angles of interest uniformly. This done by binning the arena up into a grid and then
+spinning an artifical mouse around in each bin and calculating the angle between the head and the point of interest.
+
+NOTE: 
+- Discovered negative x and y coordinates for the min head direction points which is not possible. 
+Leaving this here as a reminder that we need more tests and quality checks in the pipeline as the pipeline is untested and unreliable in it's current state 
+    
+TODO:
+- Currently the logic is not condition specific so will fail for mushroom
+- The logic is not robust in the sense that it does not prevent the synthetic mouse from walking through the barrier
+This should not be a too big of an issue and should only have minimal impact on the marginals
+- Make util functions for removing points away from the center global as been used 3x now in individual scripts
+
+"""
+
+# Import OS Lib
+
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+import pandas as pd
+import seaborn as sns
+import polars as pl
+import os
+
+# Import custom lib
+
+from settings.settings_visualize import defined_settings_visualize as settings
+
+def plot_all_marginal_dists(trackingData, videoDf, sessionHeight, save_path) -> None:
+    """
+    This function is the main function that calls all of the other functions in this script. It plots all of the marginals
+    and their corresponding optimal marginals. The optimal marginal for head direction is not plotted as it is manually generated
+    and thus is just a function of how we choose to generate the data.
+    
+    NOTE - I think the right edge is north and the left edge is south.
+    """
+    
+    # Retrieve data
+    videoDf = videoDf.filter(pl.col("OutofshelterIdx") == True)
+    optimalMarginals = create_optimal_marginal_distributions(trackingData, sessionHeight)
+    
+    # Define plot
+    fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(24, 6), sharey=True, sharex=True)
+    
+    # Head Direction
+    sns.kdeplot(data=videoDf, x="hdir", ax=axs[0], cut=0, fill = True)
+    axs[0].set_title("Head Direction PDF")
+    axs[0].legend(['Empirical Marginal'])
+    
+    # Head shelter angle
+    sns.kdeplot(data=videoDf["hsa"].to_pandas(), ax=axs[1], cut=0, fill = True, common_norm=False)
+    sns.kdeplot(data=optimalMarginals["shelterLocation"], ax=axs[1], cut=0, fill = True, color='darkorchid', common_norm=False)
+    axs[1].set_title("Shelter Angle PDF")
+    axs[1].legend(['Empirical Marginal', 'Optimal Marginal'])
+
+    # Right edge angle
+    sns.kdeplot(data=videoDf["h_bar_north_a"].to_pandas(), ax=axs[2], cut=0, fill = True, common_norm=False)
+    sns.kdeplot(data=optimalMarginals["rightEdgeLocation"], ax=axs[2], cut=0, fill = True, color='darkorchid', common_norm=False)
+    axs[2].set_title("Rigth Edge Angle PDF")
+    axs[2].legend(['Empirical Marginal', 'Optimal Marginal'])
+    
+    # Left edge angle
+    sns.kdeplot(data=videoDf["h_bar_south_a"].to_pandas(), ax=axs[3], cut=0, fill = True, common_norm=False)
+    sns.kdeplot(data=optimalMarginals["leftEdgeLocation"], ax=axs[3], cut=0, fill = True, color='darkorchid', common_norm=False)
+    axs[3].set_title("Left Edge Angle PDF")
+    axs[3].legend(['Empirical Marginal', 'Optimal Marginal'])
+    
+    # Settings for all axes
+    for ax in axs.flat:
+        ax.set_xlim([-np.pi, np.pi])
+        ax.set_xlabel("Angle (radians)")
+        ax.set_ylabel("Density")
+    plt.subplots_adjust(wspace=0.05, hspace=0)
+    if settings.show_plots: plt.show()
+    plt.savefig(os.path.join(save_path, "Emperical_Marginals_VS_Optimal_Marginals.png"))
+    plt.close()
+    
+def create_optimal_marginal_distributions(trackingData, session_height) -> dict:
+    """ 
+    A function that generates the optimal marginals for each point of interest in the arena.
+    
+    Returns:
+    - a dictionary of marginals for four points of interest in the arena. Although
+    the central barrier location is not used in the analysis it is still generated
+    """
+    
+    pointsOfInterest = define_points_of_interest(trackingData)
+    xEdges, yEdges =  bin_the_data_create_edges(trackingData)
+    positionPoints = generate_position_points(xEdges, yEdges, session_height)
+    dataFrame = generate_hdir_data_for_each_position_point(positionPoints)
+    str = ["leftEdgeLocation", "centerBarrierLocation", "rightEdgeLocation", "shelterLocation"]
+    unNormalizedMarginal = {}
+    for idx, pointOfInterest in enumerate(pointsOfInterest):
+        unNormalizedMarginal[str[idx]] = compute_the_angle_between_the_head_and_a_point(dataFrame, pointOfInterest)
+    return unNormalizedMarginal
+
+def define_points_of_interest(trackingData) -> list:
+    """ 
+    Returns a list of tuples, each tuple is a point of interest in the arena. As there
+    are two shelter points, top left edge and bottom right edge of the shelter the shelter differs in logic.
+    """
+    
+    leftEdgeLocation = trackingData["barrier_loc"][0] # xy
+    centerBarrierLocation = trackingData["barrier_loc"][1] # xy
+    rightEdgeLocation = trackingData["barrier_loc"][2] # xy
+    shelterLocation = (np.mean([trackingData["shelter_loc"][0][0], trackingData["shelter_loc"][1][0]]), # There are two shelter points, top left edge and bottom right edge of the shelter
+                       np.mean([trackingData["shelter_loc"][0][1], trackingData["shelter_loc"][1][1]])) # xy
+    pointsOfInterest = [leftEdgeLocation, centerBarrierLocation, rightEdgeLocation, shelterLocation]
+    
+    return pointsOfInterest
+
+def bin_the_data_create_edges(trackingData, numBins = 10) -> tuple:
+    """ 
+    Bin the arena up in order to generate points of X,Y coordinates to use to generate
+    synthetic head direction points for each bin
+    """
+    
+    xCoordOfHead = trackingData['head_loc'][:, 0]
+    yCoordOfHead = trackingData['head_loc'][:, 1]
+    _, xEdges, yEdges = np.histogram2d(xCoordOfHead, yCoordOfHead, bins= numBins) # 10 bins means 100 points
+    return xEdges, yEdges
+
+def generate_position_points(xEdges, yEdges, session_height, plotPointsGenerated = False) -> list:
+    """ 
+    Returns a list of tuples of the X,Y coordinates of the center of each bin.
+    There is also an option to plot the points generated for visualisation purposes
+    that is set to False by default. 
+    """
+
+    # Tile the arena using the collected head position data
+    xCoords = (xEdges[1:] + xEdges[:-1]) / 2 # Find center of bins
+    yCoords = (yEdges[1:] + yEdges[:-1]) / 2 # Find center of bins
+    
+    # Remove any points outside of the arena
+    xCoords, yCoords = remove_points_away_from_center_of_circle(xCoords, yCoords, session_height)
+    
+    # Generate a list of tuples of the X,Y coordinates of the center of each bin
+    positionPoints = [[x, y] for x in xCoords for y in yCoords]
+    
+    # Plot the points generated for visualisation purposes and debugging
+    if plotPointsGenerated:
+        plt.scatter(*zip(*positionPoints))
+        plt.grid(True)
+        plt.xlabel('X Coordinates')
+        plt.ylabel('Y Coordinates')
+        plt.title('Plot of Coordinate Pairs')
+        plt.show()
+    
+    return positionPoints
+
+def generate_hdir_data_for_each_position_point(positionPoints) -> pd.DataFrame:
+    """ 
+    Generate a dataframe of all possible combinations of position points and spins.
+    This is the function that generates the synthetic head direction data for each "grid" or bin in the arena
+    genrated from the bin_the_data_create_edges function.
+    """
+    
+    spins = np.arange(-np.pi, np.pi, 0.1) # Generate a list of angles from 0 to 2pi in 0.1 increments for head direction
+    data = np.array(list(itertools.product(positionPoints, spins)), dtype=object) # create an array of all possible combinations of position points and spins
+    dataFrame = pd.DataFrame(data, columns=['xY', 'hDir'])
+
+    return dataFrame
+
+def compute_the_angle_between_the_head_and_a_point(headPositionDf, pointOfInterest):
+    """ 
+    Input:
+    + headPosition, (X, Y) coordinates of the head as well as synthetic head direction
+    + pointname: the column name of the point you want to compute the angle to in the tracking data e.g barrier location, etc#
+    + idx: Some of the columns have multiple index points e.g barrier location has two points so you need to index which one you want
+        
+    Expects:
+    - angles to be in radians
+    
+    Logic:
+    - xLen and yLen are the x and y components of the triangle formed by the head and the point of interest
+    - Inverse of Tan is used to obtain the angle in radians between the x and y components, arcTan2 is used to ensure the correct quadrant is returned (Unsure why negative is needed)
+    - The pos and negative logic is to covert a 270 degree turn into a -90 degree turn
+    - Rotate the coordinate plane by 90 degrees to ensure it is in the same coordinate system as the head direction
+    - Ensure angle generated is (from pi to -pi) by wrapping values over 180 degrees back to negative
+    """
+    
+    # Use the inverse of tan to compute the angle between the head and the point of interest
+    xLen = - headPositionDf["xY"].apply(lambda x: x[0]) + pointOfInterest[0] 
+    yLen = - headPositionDf["xY"].apply(lambda x: x[1]) + pointOfInterest[1] 
+    angleOFInterest = - np.arctan2(yLen, xLen)
+
+    # Ensure a 270 degree turn is converted to a -90 degree turn
+    isAngleOFInterestPositive = angleOFInterest>0
+    isAngleOFInterestNegative = angleOFInterest<0
+    angleOFInterest[isAngleOFInterestNegative] += np.pi 
+    angleOFInterest[isAngleOFInterestPositive] -= np.pi 
+    
+    # Ensure angle generated is (from pi to -pi)
+    adjustedAngleOfInterest = np.pi + (angleOFInterest - headPositionDf['hDir']) # brackets for order of operations its not atuple
+    adjustedAngleOfInterest[adjustedAngleOfInterest > np.pi] = (adjustedAngleOfInterest[adjustedAngleOfInterest > np.pi] - (2*np.pi))
+    
+    return adjustedAngleOfInterest
+
+# Utils - This has been used 3x now, make it global!!
+def remove_points_away_from_center_of_circle(x, y, session_height) -> tuple:
+    """ 
+    Ensures there are no positions outside of the areana by removing them from the x and y coordinates based
+    on the fact that the radius of the arena is 460 pixels.
+    
+    TODO:
+    + Make this function global
+    + Make the radius of the arena a variable not hard coded
+    """
+    
+    dist = np.sqrt(((x - session_height/2)**2) + ((y - session_height/2)**2)) # Use the euclidean distance formula to find the distance from the center of the arena
+    filtX = x[dist<460] # 460 is size of arena circle radius, see register
+    filtY = y[dist<460]
+    return filtX, filtY
