938cdd7607fda691fb4602f243503d611358b279
JRegg66 <jasmine.reggiani@gmail.com>
2023-10-18 13:33:50 +0100

    all mouses record

--- behave_analysis/database/Mice/AllMouses.py
+++ behave_analysis/database/Mice/AllMouses.py
@@ -5,7 +5,7 @@ from behave_analysis.database.Mice.mouse_class import Mouse
 
 JAL001 = Mouse(
     nick_name="JAL001",
-    total_sessions=3,
+    total_sessions=9,
     mouse_number_pyrat="BAA-1102922",
     experiment_file_names=None,
     root_path = Path(r"JAL001")
@@ -13,7 +13,7 @@ JAL001 = Mouse(
 
 JAL002 = Mouse(
     nick_name="JAL002",
-    total_sessions=0,
+    total_sessions=9,
     mouse_number_pyrat="xxx-xxxxxxx",
     experiment_file_names=None,
     root_path=Path(r"JAL002")
@@ -21,7 +21,7 @@ JAL002 = Mouse(
 
 JAL003 = Mouse(
     nick_name="JAL003",
-    total_sessions=3,
+    total_sessions=11,
     mouse_number_pyrat="BAA-1103439",
     experiment_file_names=None,
     root_path = Path(r"JAL003")
@@ -29,7 +29,7 @@ JAL003 = Mouse(
 
 JAL004 = Mouse(
     nick_name="JAL004",
-    total_sessions=3,
+    total_sessions=10,
     mouse_number_pyrat="BAA-1103424",
     experiment_file_names=None,
     root_path = Path(r"JAL004")
@@ -37,7 +37,7 @@ JAL004 = Mouse(
 
 JAL005 = Mouse(
     nick_name="JAL005",
-    total_sessions=3,
+    total_sessions=9,
     mouse_number_pyrat="BAA-xxx",
     experiment_file_names=None,
     root_path = Path(r"JAL005")
