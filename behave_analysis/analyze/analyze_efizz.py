dbbfc586d0e6a90f916c05a519e3fb07011e8011
JRegg66 <jasmine.reggiani@gmail.com>
2023-11-08 09:59:59 +0000

    rayleigh sig test

--- behave_analysis/analyze/analyze_efizz.py
+++ behave_analysis/analyze/analyze_efizz.py
@@ -2,7 +2,7 @@
 
 from behave_analysis.analyze.TunED.tunED_model import TunEdModel
 # from behave_analysis.analyze.LDA.LDAmodel import run_LDA_model
-from settings.settings_analyze_efizz import Settings_analyze_efizz
+from settings.settings_analyze_efizz import Settings_ae
 from behave_analysis.analyze.linshit import LinearShift
 from behave_analysis.analyze.decoders.LSTM.LSTM_model import preprocess_data_and_set_up, main, bin_polars_dataframes
 from behave_analysis.analyze.Rayleigh.computeRayleigh import compute_all_clusters_rayleigh, compute_single_cluster_tuning
@@ -29,8 +29,8 @@ class AnalyzeEfizz:
         logger.info('Initializing AnalyzeEfizz')
         self.session = session
         self.dir = os.path.join(session.base_path, session.processed_path) + "\\" + 'models' 
-        self.show_plots = Settings_analyze_efizz.show_plots
-        self.settings = Settings_analyze_efizz
+        self.show_plots = Settings_ae.show_plots
+        self.settings = Settings_ae
         self.all_conditions = self.extract_all_or_custom_conditions(session)
         self.video_df = pl.read_csv(os.path.join(
             self.session.base_path, 
@@ -39,7 +39,7 @@ class AnalyzeEfizz:
         make_directory(self.dir)
         
         # For each cluster type in settings e.g synthetic, syntheticHdir, good, mua
-        for cType in Settings_analyze_efizz.cluster_type:
+        for cType in Settings_ae.cluster_type:
             self.cluster_type = cType
             try: #Load in postprocess object
                 fileObj = open(os.path.join(self.session.base_path, 
@@ -55,25 +55,25 @@ class AnalyzeEfizz:
 
     def extract_all_or_custom_conditions(self, session):
         """Identify all conditions to analyze or use custom conditions from settings file"""
-        if len(Settings_analyze_efizz.condition) == 0:
+        if len(Settings_ae.condition) == 0:
             conditions = identify_conditions(session)
         else:
-            conditions = Settings_analyze_efizz.condition
+            conditions = Settings_ae.condition
         return conditions
 
     def execute_models(self):
         logger.info('Executing models')
         
 # ------------------------------ Compute TUNED --------------------------------
-        if Settings_analyze_efizz.run_tunED:
+        if Settings_ae.run_tunED:
             logger.info('Running TunED model')
             if not os.path.isdir(self.dir + "\\" + "tunED"):
                 os.mkdir(self.dir + "\\" + "tunED")
             model_path = os.path.join(self.dir, 'tunED')
             TunEdModel(post_process_object = self.postprocessObject, 
-                       analyze_efizz_settings =  Settings_analyze_efizz, 
+                       analyze_efizz_settings =  Settings_ae, 
                        save_dir = model_path, 
-                       apply_linear_shift = Settings_analyze_efizz.linear_shift,
+                       apply_linear_shift = Settings_ae.linear_shift,
                        cluster_type = self.cluster_type,
                        conditions = self.all_conditions)
             logger.success('TunED analysis complete')
@@ -98,25 +98,25 @@ class AnalyzeEfizz:
         #     logger.success('LDA analysis complete')
 
 # ----------------- Compute Rayleigh and polar plots -------------------------
-        if Settings_analyze_efizz.run_rayleigh:
-            if not Settings_analyze_efizz.single_cluster_plots:
+        if Settings_ae.run_rayleigh:
+            if not Settings_ae.single_cluster_plots:
                 logger.info(f"Compute Rayleigh on {self.cluster_type} data")
                 all_angles = identify_angles(self.session)
-                if len(Settings_analyze_efizz.condition) > 0: 
-                    all_conditions = Settings_analyze_efizz.condition
+                if len(Settings_ae.condition) > 0: 
+                    all_conditions = Settings_ae.condition
                 else: 
                     all_conditions = identify_conditions(self.session)
                 base_path = os.path.join(self.dir, 
                                          'Rayleigh', 
                                          self.cluster_type)
                 compute_all_clusters_rayleigh(self, 
-                                              Settings_analyze_efizz, 
+                                              Settings_ae, 
                                               all_angles, 
                                               all_conditions, 
                                               base_path)
             else:
                 logger.info(f"Making single cluster polar plots on {self.cluster_type} data")
-                compute_single_cluster_tuning(self, Settings_analyze_efizz)
+                compute_single_cluster_tuning(self, Settings_ae)
         
         logger.success('All models complete')
             
