596fd7a0bc14890feaf4951767f2d26e21e23624
JRegg66 <jasmine.reggiani@gmail.com>
2024-01-19 18:17:53 +0000

    format bug

--- debug/debug_analyze.py
+++ debug/debug_analyze.py
@@ -6,7 +6,7 @@ from databank import experiments_objects
 from behave_analysis.process.process import Process
 from behave_analysis.analyze.analyze_efizz import AnalyzeEfizz
 from behave_analysis.analyze.AnalyzeBehave import AnalyzeBehave
-from settings.settings_analyze import Settings_analyze as settings_a
+from settings.settings_analyze import settings_analyze as settings_a
 
 def analyze():
     """
