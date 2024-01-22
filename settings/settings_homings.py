d4bb3463fd036dd51b63e3eb34203d16a1968a79
LaurenceFreeman <laurence.1.freeman@gmail.com>
2024-01-22 16:21:08 +0000

    Changing boxcar padding, future looking and smoothing type

--- settings/settings_homings.py
+++ settings/settings_homings.py
@@ -2,7 +2,7 @@ from behave_analysis.utils.settings_objects import Settings_homings
 
 settings_homings = Settings_homings(
     fast_speed=15,  # 15 cm/s seems quite slow no? In the paper philip used 10 cm/s
-    padding_duration=1,  # How long should the homing event be in seconds
+    padding_duration=0.5,  # How long should the homing event be in seconds
     fast_angular_speed=90,  # Turning towards some reference location speed
     min_change_in_dist_to_shelter=0.3,  # (maybe %) How far does the mouse have to move towards the shelter to be considered a homing event
     max_time_within_session=2000,  # How long is session in minutes - Ignore I think
