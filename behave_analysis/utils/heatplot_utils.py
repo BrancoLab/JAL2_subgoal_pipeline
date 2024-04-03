00777b414c4755627858b86bbdc4e2e37a1aba4a
LaurenceFreeman <laurence.1.freeman@gmail.com>
2024-04-03 10:31:53 +0100

    Working heatmap logic

--- behave_analysis/utils/heatplot_utils.py
+++ behave_analysis/utils/heatplot_utils.py
@@ -1,15 +1,66 @@
 import numpy as np
 
+
 def remove_points_away_from_center_of_circle(x, y, session_height) -> tuple:
     """Remove all points that are outside of the arena circle and return the filtered x and y coordinates.
- 
+
     TODO:
         + Make the radius of the arena a variable not hard coded
     """
 
-    dist = np.sqrt(
-        ((x - session_height / 2) ** 2) + ((y - session_height / 2) ** 2)
-    )  # Use the euclidean distance formula to find the distance from the center of the arena
+    dist = np.sqrt(((x - session_height / 2) ** 2) + ((y - session_height / 2) ** 2))
+    # Use the euclidean distance formula to find the distance from the center of the arena
     filt_x = x[dist < 460]  # 460 is size of arena circle radius, see register
     filt_y = y[dist < 460]
     return filt_x, filt_y
+
+
+def add_features(ax, condition: str, tracking: dict, xbins: np.array, ybins: np.array) -> None:
+    """Draw arena and barrier on top of heatmaps or scatterplots
+
+    Args:
+        ybins (np.array): ybins used to digitize the y coordinates e.g [96., 192., 288., 384., 480.]
+        xbins (np.array): xbins used to digitize the x coordinates e.g [96., 192., 288., 384., 480.]
+        tracking (dict): dictionary containing tracking data
+        condition (str): condition of the plot
+        ax (matplotlib.axes): axis object to draw on
+        
+    NOTE:
+        If tracking is outside of the arena the shelter and the barrier will not be drawn consistently. Thus
+        it is important to filter the tracking data before calling this function.
+    """
+
+    arena_radius = 460
+    # draw shelter
+    if "shelter_loc" in tracking.keys():
+        shelt = [np.digitize(tracking["shelter_loc"][0], xbins), np.digitize(tracking["shelter_loc"][1], ybins)]
+        for i in [0, 1]:
+            ax.plot([shelt[0][0], shelt[1][0]], [shelt[i][1], shelt[i][1]], color="k")
+            ax.plot([shelt[i][0], shelt[i][0]], [shelt[0][1], shelt[1][1]], color="k")
+
+    if not np.logical_or(condition == "shelter_only", condition == "pre_shelter"):
+        if len(tracking["barrier_loc"]) > 0:
+            if np.logical_or(np.logical_or(condition == "barrier_present", condition == "all_time"), condition == "shelter_present"):
+                # draw old two-sided barrier
+                bar_loc = [tracking["barrier_loc"][0][0], tracking["barrier_loc"][1][0]]
+
+            if condition == "barrier_pre_flip":
+                # draw barrier from first point to the edge
+                if tracking["barrier_loc"][0][0] < 512:
+                    bar_loc = [tracking["barrier_loc"][0][0], 512 + arena_radius]
+                else:
+                    bar_loc = [512 - arena_radius, tracking["barrier_loc"][0][0]]
+
+            if condition == "barrier_post_flip":
+                # draw barrier from second point to the edge
+                if tracking["barrier_loc"][1][0] < 512:
+                    bar_loc = [tracking["barrier_loc"][1][0], 512 + arena_radius]
+                else:
+                    bar_loc = [512 - arena_radius, tracking["barrier_loc"][1][0]]
+
+            bar_loc = np.digitize(bar_loc, xbins)
+            ax.plot(
+                [bar_loc[0], bar_loc[1]],
+                [np.digitize(tracking["barrier_loc"][0][1], ybins), np.digitize(tracking["barrier_loc"][1][1], ybins)],
+                color="k",
+            )
--- behave_analysis/visualize/efizz/heatmap.py
+++ behave_analysis/visualize/efizz/heatmap.py
@@ -11,34 +11,68 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
+from loguru import logger
+from tqdm import tqdm
 
+from behave_analysis.visualize.visualize_utils import open_tracking_data
 from behave_analysis.utils.creating_directories import make_directory
 from behave_analysis.analyze.filtering_data.filtering_functions import filter_video_dataframe
 from scipy.ndimage import gaussian_filter
+from behave_analysis.utils.heatplot_utils import add_features
 
-def single_unit_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, save_base) -> None:
+# Because of memory constraints, let's only keep the columns we need for this module
+COLUMNS_TO_KEEP = [
+    "mouse_x_position",
+    "mouse_y_position",
+    "spike_clusters",
+    "spike_count",
+    "OutofshelterIdx",
+    "EscapePeriod",
+    "shelter",
+    "barrier_present",
+    "barrier_flipped",
+]
+
+
+def single_unit_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, save_base, session) -> None:
     """"""
+
+    logger.info("Creating single unit heatmaps...")
+    tracking_data = open_tracking_data(session)
     save_path = make_directory(os.path.join(save_base, "single_unit_heatmaps"))
-    unit_ids = video_and_spike_data["spike_clusters"].unique().to_numpy()
-    fig, axs = plt.subplots(nrows=1, ncols=len(conditions), figsize=(15, 15), sharey=True, sharex=True)
-    cbar_ax = fig.add_axes([0.91, 0.13, 0.01, 0.75])  # The list represents [left, bottom, width, height],
+    data = video_and_spike_data.select(COLUMNS_TO_KEEP)  # Prevents memory issues and computer crashes
+    del video_and_spike_data  # Release reference to the original DataFrame
+    unit_ids = data["spike_clusters"].unique().to_numpy()
+
+    # Remove all rows that are outside of the arena circle
+    data = data.with_columns(
+        np.sqrt(((pl.col("mouse_x_position") - session.video.height / 2) ** 2) + ((pl.col("mouse_y_position") - session.video.height / 2) ** 2)).alias("dist")
+    )
     
-    for clu in unit_ids:    
+    # Now filter out the mouse x positions where distance is less than 460 and same for y
+    data = data.filter(pl.col("dist") < 460)
+    data = data.drop("dist")
+    
+    for clu in unit_ids:
+        fig, axs = plt.subplots(nrows=1, ncols=len(conditions), figsize=(15, 10), sharey=True, sharex=True)
+        cbar_ax = fig.add_axes([0.91, 0.6, 0.01, 0.2])  # The list represents [left, bottom, width, height]
+        logger.info(f"Creating heatmap for unit {clu}")
         for idx, condition in enumerate(conditions):
-            filtered_df = filter_video_dataframe(dataframe=video_and_spike_data, condition=condition)
+            filtered_df = filter_video_dataframe(dataframe=data, condition=condition)
             clu_df = filtered_df.filter(pl.col("spike_clusters") == clu)
-            
-            if pddf.empty:  # Skip if no data for this unit and condition
+
+            if clu_df.is_empty():  # Skip if no data for this unit and condition
                 continue
-            
-            if sum(pddf["spike_count"]) == 0:  # Skip if no spikes for this unit and condition
+
+            if sum(clu_df["spike_count"]) == 0:  # Skip if no spikes for this unit and condition
                 continue
-            
+
             pddf = clu_df.to_pandas()
-            
+
             # Bin the x and y positions
-            x_bins = pd.cut(pddf["mouse_x_position"], bins=20, labels=False)  # Adjust bins as needed
-            y_bins = pd.cut(pddf["mouse_y_position"], bins=20, labels=False)  # Adjust bins as needed
+            x_bins, x_bin_nums = pd.cut(pddf["mouse_x_position"], bins=30, labels=False, retbins=True)  # Adjust bins as needed
+            y_bins, y_bin_nums = pd.cut(pddf["mouse_y_position"], bins=30, labels=False, retbins=True)  # Adjust bins as needed
+
             pddf["x_bins"] = x_bins
             pddf["y_bins"] = y_bins
 
@@ -51,40 +85,36 @@ def single_unit_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, s
             # Merge the aggregated spike counts with the bin counts
             normalized_df = pd.merge(aggregated_df, bin_counts, on=["x_bins", "y_bins"])
 
-            # Normalize the spike counts by the number of entries in each bin
-            normalized_df["normalized_spike_count"] = normalized_df["spike_count"] / normalized_df["total_entries"] * 40
+            # Normalize the spike counts by the number of entries in each bin - where 40 is frames per second
+            normalized_df["normalized_spike_count"] = (normalized_df["spike_count"] / normalized_df["total_entries"]) * 40
 
             # Pivot the DataFrame for heatmap creation
             # Y bins are the rows, x bins are the columns and the values are firing rates
-            pivot_df = normalized_df.pivot(index = "y_bins", columns = "x_bins", values = "normalized_spike_count").fillna(0)
-            
-            # Apply Gaussian smoothing
-            if pivot_df.to_numpy().sum() > 0:  # Check if there's any data to smooth
-                smoothed_data = gaussian_filter(pivot_df, sigma=0.2)
-                sns.heatmap(smoothed_data, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs[idx], mask=(smoothed_data == 0))
-                pivot_df = smoothed_data
-            else:
-                sns.heatmap(pivot_df, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs[idx], mask=(pivot_df == 0))
-            
+            pivot_df = normalized_df.pivot(index="y_bins", columns="x_bins", values="normalized_spike_count").fillna(0)
+
             # Create the heatmap
-            axs[idx] = sns.heatmap(pivot_df, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs[idx],  mask=(pivot_df == 0))
+            add_features(axs[idx], condition, tracking_data, x_bin_nums, y_bin_nums)
+            # axs[idx] = sns.heatmap(pivot_df, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs[idx], mask=(pivot_df == 0))
+            axs[idx] = sns.heatmap(
+                pivot_df,
+                cmap="coolwarm",
+                robust=True,
+                cbar_ax=cbar_ax,
+                ax=axs[idx],
+                mask=(pivot_df == 0),  # Mask zero values
+            )
             axs[idx].set_xticklabels([])
             axs[idx].set_yticklabels([])
+            axs[idx].set_title(condition, fontsize=20)
             axs[idx].xaxis.set_ticks_position("none")
             axs[idx].yaxis.set_ticks_position("none")
             axs[idx].set_aspect("equal")
-        
+            cbar_ax.set_label("")  # Colour bar is last axis
+
         fig.suptitle(f"Unit {clu}: heatmap", fontsize=20)
         file_name = f"unit{clu}_heatmap.png"
         fig.savefig(save_path + "\\" + file_name)
-            
-        # Exciplit memory management
-        # del filtered_df, pddf, x_bins, y_bins, aggregated_df, bin_counts, normalized_df, pivot_df
-        # gc.collect()
         plt.clf()
-        plt.close('all')
-    
-    
 
 
 def population_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, save_base) -> None:
@@ -98,11 +128,11 @@ def population_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, sa
         conditions (list): A list of conditions to divide the heatmap into, each condition is a string
         save_base (str): The base directory to save the heatmaps to
     """
-    
+
     save_path = make_directory(os.path.join(save_base, "population_heatmaps"))
 
     for _, condition in enumerate(conditions):
-        
+
         # Filter_video_dataframe is a function that filters the dataframe based on the given condition
         filtered_df = filter_video_dataframe(dataframe=video_and_spike_data, condition=condition)
         pddf = filtered_df.to_pandas()
@@ -127,16 +157,12 @@ def population_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, sa
 
         # Pivot the DataFrame for heatmap creation
         # Y bins are the rows, x bins are the columns and the values are firing rates
-        pivot_df = normalized_df.pivot(index = "y_bins", columns = "x_bins", values = "normalized_spike_count").fillna(0)
-        
-        # Apply Gaussian smoothing
-        sigma = 0.2  # Standard deviation for Gaussian kernel. Adjust as needed.
-        smoothed_data = gaussian_filter(pivot_df, sigma=sigma)
-        
+        pivot_df = normalized_df.pivot(index="y_bins", columns="x_bins", values="normalized_spike_count").fillna(0)
+
         # Create the heatmap
         fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(15, 15), sharey=True, sharex=True)
         cbar_ax = fig.add_axes([0.91, 0.13, 0.01, 0.75])  # The list represents [left, bottom, width, height],
-        axs = sns.heatmap(smoothed_data, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs,  mask=(pivot_df == 0))
+        axs = sns.heatmap(pivot_df, cmap="coolwarm", robust=True, cbar_ax=cbar_ax, ax=axs, mask=(pivot_df == 0))
         axs.set_xticklabels([])
         axs.set_yticklabels([])
         axs.xaxis.set_ticks_position("none")
@@ -145,9 +171,9 @@ def population_level_heatmaps(video_and_spike_data: pl.DataFrame, conditions, sa
         axs.set_aspect("equal")
         file_name = f"population_heatmap_{condition}.png"
         fig.savefig(save_path + "\\" + file_name)
-        
+
         # Exciplit memory management
         del filtered_df, pddf, x_bins, y_bins, aggregated_df, bin_counts, normalized_df, pivot_df, smoothed_data
         gc.collect()
         plt.clf()
-        plt.close('all')
+        plt.close("all")
