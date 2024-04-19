bae2c8d8a60d7a3c212fb596ce92c8ef86d12aad
JRegg66 <jasmine.reggiani@gmail.com>
2024-04-19 12:02:53 +0100

    winstor or ceph path assign

--- behave_analysis/database/computer_ID.py
+++ behave_analysis/database/computer_ID.py
@@ -1,19 +1,26 @@
+import os
 import socket
 import tkinter as tk
 from tkinter import filedialog
+from loguru import logger
 
-def get_computer_specific_paths():
+def get_computer_specific_paths(session_path = ''):
 
     hostname = socket.gethostname()
+    base_path = ''
 
     if hostname == "W10-221I": # Jasmine machine
-        # base_path = r"E:\Experimental_Data"
-        base_path = r"Z:\Jasmine_Laurence\Experimental_Data"
+        my_machine = r"E:\Experimental_Data"
+        ceph_path = r"Z:\Jasmine_Laurence\Experimental_Data"
+        winstor_path = r"W:\branco\Laurence"
         DLC_path = r"C:\Users\jreggiani\Documents\DLC\JAL_NPX1-Jasmine-2023-03-22\config.yaml"
+    
     elif hostname == "DESKTOP-FBQJ1VU": #Laurence machine
-        base_path = r"E:\efizz"
-        # base_path = r"D:\efizz"
+        ceph_path = r"Z:\Jasmine_Laurence\Experimental_Data"
+        winstor_path = r"W:\branco\Laurence"
+        # my_machine = r"D:\efizz"
         DLC_path = r"D:\DLC\JAL_NPX1-Jasmine-2023-03-22\config.yaml"
+    
     else: # unknown machine
         root = tk.Tk()
         root.withdraw()  # Hide the main window
@@ -21,5 +28,14 @@ def get_computer_specific_paths():
         root = tk.Tk()
         root.withdraw()  # Hide the main window
         DLC_path = filedialog.askopenfilename(title="Select DLC config.yaml to be used for tracking")
+
+    if len(base_path) == 0:
+        # check where your folder lives and assign base path accordingly
+        if os.path.exists(os.path.join(ceph_path,session_path)):
+            base_path = ceph_path
+        elif os.path.exists(os.path.join(winstor_path,session_path)):
+            base_path = winstor_path
+        else:
+            logger.warning("You sessions is not on winstor or ceph, please check paths!")
 
     return base_path, DLC_path
