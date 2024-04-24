c53b5be1f65ef7b939ea4ddbeecbd9c84be8b51f
JRegg66 <jasmine.reggiani@gmail.com>
2024-04-24 09:07:19 +0100

    the endlesd list vs int problem

--- behave_analysis/utils/generate_stim_status_array.py
+++ behave_analysis/utils/generate_stim_status_array.py
@@ -5,7 +5,9 @@ def generate_stim_status_array(onset_frames: np.int64, stimulus_durations, secon
     """No idea what this function does, but it generates a numpy array of some sort. Adding an assert because
     it seems to fail for escapes but works for homings. Need to change the output of the onsets for escapes."""
     
-    assert isinstance(onset_frames, np.int64), "onset_frames must be an integer"
+    # assert isinstance(onset_frames, np.int64), "onset_frames must be an integer"
+    if not isinstance(onset_frames, np.int64):
+        onset_frames = int(onset_frames)
 
     stim_status = np.zeros((0) + int((seconds_before + stimulus_durations[-1] + seconds_after) * fps)) + 0.01  # 0.01 ~ in between stimuli
     stim_status[: seconds_before * fps] = np.arange(-seconds_before * fps - 1, -1) / fps  # pre-stimulus countdown in seconds
