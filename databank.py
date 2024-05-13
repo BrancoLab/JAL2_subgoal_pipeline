4086fb858aff276bfbf3a98ded1029a53501c66b
JRegg66 <jasmine.reggiani@gmail.com>
2024-05-13 16:41:29 +0100

    add JAL8 sesh

--- databank.py
+++ databank.py
@@ -27,7 +27,7 @@ from behave_analysis.database.Experiments.JAL006_ex import JAL6_hab_1mar, JAL6_s
 
 from behave_analysis.database.Experiments.JAL007_ex import JAL7_hab_1mar, JAL7_empty_shelter, JAL7_sesh8_9apr
 
-from behave_analysis.database.Experiments.JAL008_ex import JAL8_shelt_22apr
+from behave_analysis.database.Experiments.JAL008_ex import JAL8_shelt_22apr, JAL8_flip1_25apr, JAL8_flip2_29apr, JAL8_tiny_3may
 
 # ## -------------JR BEHAVIOR MICE
 
@@ -35,5 +35,5 @@ from behave_analysis.database.Experiments.Burrow_ex import burrow_3456, burrow_3
 
 # Currently the code only works with one experiment at a time, so place that experiment in the below list for analysis
 
-experiments_objects = [JAL4_3rdSept]
+experiments_objects = [JAL8_flip1_25apr]
 
