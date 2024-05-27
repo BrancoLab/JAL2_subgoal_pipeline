111b83e0f2eb3afac4c28f6af5bfd2a5f46fd4af
JRegg66 <jasmine.reggiani@gmail.com>
2024-05-27 13:17:18 +0100

    databank

--- behave_analysis/database/Experiments/JAL007_ex.py
+++ behave_analysis/database/Experiments/JAL007_ex.py
@@ -19,7 +19,7 @@ JAL7_hab_1mar = Experiment(  # Mouse specific
     experiment_path=Path(r"JAL007_Habituation_2024_03_01T14_13_42"),
 )
 
-JAL7_empty_shelter = Experiment(  # Mouse specific
+JAL7_empty_shelter_5mar = Experiment(  # Mouse specific
     nick_name=mouse.nick_name,
     total_sessions=mouse.total_sessions,
     mouse_number_pyrat=mouse.mouse_number_pyrat,
@@ -34,6 +34,40 @@ JAL7_empty_shelter = Experiment(  # Mouse specific
     barrier_time=[],
     barrier_flip_time=None,
     experiment_path=Path(r"JAL007_empty_shelter_2024_03_05T13_45_47"),
+)
+
+JAL7_flip2_12mar = Experiment(  # Mouse specific
+    nick_name=mouse.nick_name,
+    total_sessions=mouse.total_sessions,
+    mouse_number_pyrat=mouse.mouse_number_pyrat,
+    experiment_file_names=mouse.experiment_file_names,
+    root_path=mouse.root_path,
+    # Experiment specific
+    experiment_name="sesh8",
+    experiment_idx=0,
+    experiment_date="2024_03_12",
+    experiment_time="11_18_26",
+    shelter_time=[.25, -1],
+    barrier_time=[59.5, -1],
+    barrier_flip_time= 189,
+    experiment_path=Path(r"JAL007_barrierflip2_2024_03_12T11_18_26"),
+)
+
+JAL7_flip5_22mar = Experiment(  # Mouse specific
+    nick_name=mouse.nick_name,
+    total_sessions=mouse.total_sessions,
+    mouse_number_pyrat=mouse.mouse_number_pyrat,
+    experiment_file_names=mouse.experiment_file_names,
+    root_path=mouse.root_path,
+    # Experiment specific
+    experiment_name="sesh8",
+    experiment_idx=0,
+    experiment_date="2024_03_22",
+    experiment_time="11_15_43",
+    shelter_time=[.5, -1],
+    barrier_time=[60.75, -1],
+    barrier_flip_time=214.5,
+    experiment_path=Path(r"JAL007_shelter_barrier_flip_5_2024_03_22T11_15_43"),
 )
 
 JAL7_sesh8_9apr = Experiment(  # Mouse specific
