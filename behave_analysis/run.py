b2c7d4f7ab33ce746203c2ea6430619d965144b1
JRegg66 <jasmine.reggiani@gmail.com>
2024-05-28 09:31:45 +0100

    oops

--- behave_analysis/run.py
+++ behave_analysis/run.py
@@ -103,9 +103,9 @@ def analyze():
         if settings_a.efizz:
             for c_type in Settings_ae.cluster_type:
                 
-                AnalyzeEfizz(session, c_type).classify_cells()
+                AnalyzeEfizz(session, c_type).execute_models()
                 if Settings_ae.classify_cells:
-                    AnalyzeEfizz(session, c_type).execute_models()
+                    AnalyzeEfizz(session, c_type).classify_cells()
                 
                 
     logger.success("Analysis pipeline complete")
