0d5ac2c1c452d656ec3f8a386f80cfade43f6d90
LaurenceFreeman <laurence.1.freeman@gmail.com>
2024-07-08 14:56:38 +0100

    Set everything to normal / false

--- settings/settings_analyze_efizz.py
+++ settings/settings_analyze_efizz.py
@@ -16,11 +16,11 @@ from behave_analysis.utils.settings_objects import Settings_analyze_efizz
 
 Settings_ae = Settings_analyze_efizz(
     # ------------- General settings --------------------------
-    stim_type="None",  # 'audio', leave as 'None' if no stims were delivered
+    stim_type="audio",  # 'audio', leave as 'None' if no stims were delivered
     linear_shift=False,  # whether to run linear shift!
     # This does not effect Tuned, this model needs linear shift to work
     redo_compute=False,  # if True it will force recompute any pre-saved analyses (e.g. Rayleigh and LDA)
-    cluster_type=['good'],  # ['synthetic','synthetichdir','all','good'], # Can list multiple!
+    cluster_type=["good"],  # ['synthetic','synthetichdir','all','good'], # Can list multiple!
     show_plots=False,
     # possible experimental condition inputs: 'all_time' (don't filter based on shelter or barrier),
     #                                         'pre_shelter' (empty arena),
@@ -36,15 +36,13 @@ Settings_ae = Settings_analyze_efizz(
     condition_types="experimental_conditions",  # if 'experimental_conditions' it uses conditions listed above that start with user
     # if 'behavioral_conditions' it defines the conditions based on homing/escape behaviour of mousie - it will overrule other condition settings
     # if 'homing_number_2' it defines the conditions before and after a certain number of correct homings
-    compartment_split=['by_position'], # ['all','threat_zone','shelter_compartment','left_arena','right_arena']
+    compartment_split=["all"],  # ['all','threat_zone','shelter_compartment','left_arena','right_arena']
     # If 'all' it will run the model on all data, if 'threat_zone' it will only run on the threat zone data e.g
     # If 'by_position', it will compute LDA decoding by arena position
     number_of_bins=9,  # number of bins for angles, e.g. 13 or 19 are good numbers
-    classify_cells = False,
-
+    classify_cells=False,
     # ------------------- Run single trial analysis -------------------
     run_single_trial=False,
-
     # ------------- PCA model settings --------------------------
     run_dim_reduction=False,
     run_pca=False,
@@ -58,20 +56,20 @@ Settings_ae = Settings_analyze_efizz(
     # --------------LSTM model settings ------------------------
     run_LSTM=False,
     # ------------- LDA model settings --------------------------
-    run_LDA='all_angles',  # if [] it will not run LDA
+    run_LDA=[],  # if [] it will not run LDA
     # if 'all_angles', 'all_distance','all_vectors' it will run it for all possible angles, distances, vectors
     # else:  list of angles ['hsa','hdir','h_bar_south_a','h_bar_north_a','h_bar_centre_a', 'randP']
     epoch_num=6,  # number of epochs for cross validation
     use_firing_rate=True,
     discriminant_type="linear",  # 'linear' or 'quadratic' or 'LSTM'
-    exclude_proximal = 10, # this determines how far the mouse has to be from each point for head angle point decoding, if 0 LDA uses all head angles regardless of distance to the target
-    exclude_hdir = False,
-    dropout = False, # this will iteratively dropout each cluster and recompute the LDA prediction accuracy to see how much that cluster matters
+    exclude_proximal=10,  # this determines how far the mouse has to be from each point for head angle point decoding, if 0 LDA uses all head angles regardless of distance to the target
+    exclude_hdir=False,
+    dropout=False,  # this will iteratively dropout each cluster and recompute the LDA prediction accuracy to see how much that cluster matters
     PCA_process=[],  # numnber of PCs to use, if left empty it will run without PCA
     # ------------ Rayleigh model settings ----------------------
     run_rayleigh=False,
     rayleigh_significance="linshit",  # "linshit" or "bootstrap"
-    single_cluster_plots=True,  # True: Plot every condition in one figure
+    single_cluster_plots=False,  # True: Plot every condition in one figure
     # False: Do not plot every condition in one figure for each cluster
     multi_cluster_plots=False,  # True: Plot every cluster in one figure for one condition
     # False: Do not plot every cluster in one figure
