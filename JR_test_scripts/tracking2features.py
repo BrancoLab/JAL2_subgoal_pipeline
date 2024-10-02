22251a38bf57dc96de9ba7e120f12650cd22c8f6
Jasmine Reggiani <jasmine.reggiani@gmail.com>
2024-10-02 16:13:03 +0100

    multi sesh LDA plotting

--- JR_test_scripts/tracking2features.py
+++ JR_test_scripts/tracking2features.py
@@ -1,5 +1,8 @@
 import numpy as np
+import os
+import dill as pickle
 
+from behave_analysis.process.session import get_experiment 
 
 '''From tracking data, extract random point positions and assign them to shelter or barrier'''
 
@@ -42,5 +45,88 @@ def tracking_to_features(tracking_data):
         features[feature_names[i]] = np.logical_and(np.logical_and(x>=barrier_x[0],x<=barrier_x[1]),np.logical_and(y>=barrier_y[0],y<=barrier_y[1]))
         heatmap[x[features[feature_names[i]]], y[features[feature_names[i]]]] = np.ones(shape=(np.sum(features[feature_names[i]])))*(3+i)
 
-
     return features
+
+def load_prediction_accuracy(session,settings, cond, time_cond):
+
+    coef_path = os.path.join(session.base_path,
+                            session.processed_path,
+                            'models',
+                            'LDA',
+                            settings,
+                            r'good',
+                            time_cond,
+                            'all',
+                            cond,
+                            str('good_' + cond + '_LDA_pa.pkl'))
+    if not os.path.exists(coef_path):
+        print("path still doesn't exist: " + coef_path)
+    with open(coef_path, "rb") as dill_file:
+        coef = pickle.load(dill_file)
+    
+    return coef
+
+def extract_pa_across_sesh(all_angles,experiments_objects, conditions, all_features, settings, bar_angles = True, time_cond = ['experimental_conditions']):
+
+    sessy = {}
+    all_names = []
+    pa = {}
+    avg_pa = {}
+    for a in all_angles:
+        pa[a] = np.empty((len(experiments_objects),len(conditions),len(time_cond)))
+    for a in all_features:
+        avg_pa[a] = np.empty((len(experiments_objects), len(conditions),len(time_cond)))
+
+    if isinstance(time_cond,str):
+        time_cond = [time_cond]
+
+    for s,sesh in enumerate(experiments_objects):
+        # find session
+        session = get_experiment(sesh)
+        
+        # load tracking
+        track_path = os.path.join(session.base_path,session.processed_path,r"fully_processed_tracking_data.pickle")
+        with open(track_path, "rb") as dill_file:
+            tracking_data = pickle.load(dill_file)
+        features = tracking_to_features(tracking_data)
+
+        # load prediction accuracy
+        coef = {}
+        for h_idx,h in enumerate(time_cond): # this has to be a list!
+            for idx,c in enumerate(conditions):
+                
+                point_grid = []
+
+                coef[c] = load_prediction_accuracy(session, settings, c, h)
+                
+                for a in coef[c].keys():
+                    if a in all_angles:
+                        pa[a][s,idx,h_idx] = coef[c][a]
+                    if 'rand' in a:
+                        point_grid.append(coef[c][a])
+                        
+                if bar_angles:
+                    b = [b[0] for b in tracking_data['barrier_loc']]
+                    if b[0] > b[1]: # preflip barrier on the right
+                        pa['h_rightbar_a'][s,idx,h_idx] = coef[c]['h_preflipbar_a']
+                        pa['h_leftbar_a'][s,idx,h_idx] = coef[c]['h_postflipbar_a']
+                    else:
+                        pa['h_rightbar_a'][s,idx,h_idx] = coef[c]['h_postflipbar_a']
+                        pa['h_leftbar_a'][s,idx,h_idx] = coef[c]['h_preflipbar_a']
+                
+                # avrage random point pred.acc. for shelter and barrier
+                point_grid = np.array(point_grid)
+                for a in features.keys():
+                    avg_pa[a][s,idx,h_idx] = np.mean(point_grid[features[a]])                        
+
+        name = sesh.nick_name + '_' + sesh.experiment_date
+        all_names.append(name)
+        sessy[name] = coef
+
+    if len(time_cond) == 1:
+        for a in pa.keys():
+            pa[a] = pa[a][:,:,0]
+        for a in features.keys():
+            avg_pa[a] = avg_pa[a][:,:,0]
+
+    return sessy, all_names, avg_pa, pa
