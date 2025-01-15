0cc463b4324ddbce82e45dc8bb7ab39c790262fd
LaurenceFreeman <laurence.1.freeman@gmail.com>
2025-01-15 19:23:19 +0000

    Refactor

--- behave_analysis/analyze/overview/homing_analysis/decoding_spatial_eff/model.py
+++ behave_analysis/analyze/overview/homing_analysis/decoding_spatial_eff/model.py
@@ -3,6 +3,7 @@ import pickle
 from collections import defaultdict
 from pathlib import Path
 
+from loguru import logger
 import pandas as pd
 import scipy.stats as stats
 import numpy as np
@@ -14,6 +15,100 @@ import matplotlib.pyplot as plt
 
 from behave_analysis.utils.creating_directories import make_directory
 from behave_analysis.analyze.overview.homing_analysis.decoding_spatial_eff.data_gen import load_video_data
+
+
+# ---------------------------- MAIN FUNCTION ------------------------------
+
+
+def compute_accuracy_data(data_type, data_type_name, experiments_objects, random_labels=False):
+    """Compute and analyze decoding accuracy data using logistic regression for a set of sessions.
+
+    This function processes session data, performs logistic regression with group k-fold validation,
+    and computes metrics such as accuracy, Kolmogorov-Smirnov statistics, and model coefficients.
+    It also handles class imbalance and can optionally shuffle labels to evaluate performance on random labels.
+
+    Args:
+        data_type (dict): A dictionary where keys are session names and values are session-specific data,
+                          including the design matrix and classes.
+        data_type_name (str): A string representing the name of the data type, used for saving results.
+        experiments_objects (list): A list of experiment objects corresponding to each session in `data_type`.
+        random_labels (bool, optional): If True, shuffle class labels for randomization testing. Defaults to False.
+
+    Returns:
+        tuple: A tuple containing:
+            - whole_homing (dict): A dictionary mapping session names to their mean accuracy scores.
+            - ks_decoding_comparison (defaultdict): Nested dictionaries containing Kolmogorov-Smirnov statistics
+                                                    and accuracies for each fold of each session.
+            - coefs (defaultdict): Nested dictionaries containing model coefficients for each fold of each session.
+
+    Workflow:
+        1. Create directories for saving results.
+        2. Loop through sessions to:
+           - Load and preprocess data (handle class imbalance if necessary).
+           - Perform logistic regression with group k-fold validation.
+           - Compute accuracy and other metrics for each fold.
+        3. Save results (accuracy, KS statistics, and coefficients) to a file.
+
+    Notes:
+        - Class imbalance is addressed before splitting the data.
+        - Behavioral splits and class distributions are plotted and saved for analysis.
+        - Sessions with insufficient data or class diversity are skipped.
+    """
+    save_base = r"Z:\Jasmine_Laurence\single_trial_overview"
+    save_path = os.path.join(save_base, "decoding_spatial_efficiency", data_type_name)
+    dir = make_directory(save_path)
+    whole_homing = defaultdict(list)
+    ks_decoding_comparison = defaultdict(defaultdict)
+    coefs = defaultdict(defaultdict)
+
+    for (session_name, data), experiment in zip(data_type.items(), experiments_objects):
+        logger.info(f"Running logistic regression for session: {session_name}")
+        video_df = load_video_data(experiment)
+        design_matrix = data["design_matrix"]  # The X data
+        classes_extended = np.asarray(data["classes_extended"])  # The y data
+        if random_labels:
+            np.random.shuffle(classes_extended)  # randomly shuffle the classes
+        xBalanced, y_balanced, groups_balanced = handle_class_imbalance(classes_extended, session_name, design_matrix, data)
+        if xBalanced is None:  # If the cuttoff for the amount of data has not been met
+            continue  # Skip this session
+
+        group_kfold = GroupKFold(n_splits=2)  # k-fold interator variation with non-overlapping groups
+        fold_accuracies = []
+
+        # Assign and remove the last column of the design matrix - Frame numbers are needed to access the behaviour data
+        frame_numbers = xBalanced[:, -1].astype(int)  # Access the last column of the design matrix
+        xBalanced = xBalanced[:, :-1]  # Remove the last column of the design matrix
+
+        for fold, (train_index, test_index) in enumerate(group_kfold.split(xBalanced, y_balanced, groups_balanced)):
+            X_train, X_test = xBalanced[train_index], xBalanced[test_index]
+            y_train, y_test = y_balanced[train_index], y_balanced[test_index]
+            if not check_there_are_two_classes(y_train):
+                break
+            down_count_ans = count_class_labels(y_train)
+            logger.info(f"Class distribution for fold: {fold}. 0: {down_count_ans[0]}, 1: {down_count_ans[1]}")
+            model = LogisticRegression(
+                class_weight="balanced",
+                random_state=1337,
+                max_iter=10000,
+            ).fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            fold_accuracies.append(accuracy)
+            # plot_the_train_test_behavioural_split(video_df, frame_numbers, train_index, test_index, dir, session_name, fold, accuracy)
+            x_ks, y_ks, s_ks, h_ks = plot_class_distributions_and_compute_kolmogorov_Smirnov(
+                frame_numbers, video_df, y_train, dir, session_name, fold, accuracy
+            )
+            ks_decoding_comparison[session_name][fold] = {"x": x_ks, "y": y_ks, "speed": s_ks, "hdir": h_ks, "accuracy": accuracy}
+            coefs[session_name][fold] = model.coef_[0]
+
+        whole_homing[session_name] = np.mean(fold_accuracies)
+        logger.info(f"Mean accuracy for session {session_name}: {whole_homing[session_name]}")
+        save_data = {"accuracy_data": whole_homing, "ks_data": ks_decoding_comparison, "coefs": coefs}
+
+    with open(dir / Path(f"{data_type_name}_accuracy_ks_coeffs_data.pkl"), "wb") as f:
+        pickle.dump(save_data, f)
+
+    return save_data
 
 
 # - Helper functions for the model -
@@ -40,14 +135,10 @@ def downsample_larger_class(design_matrix, classes_extended, random_state, homin
         class_1_indices = resample(class_1_indices, replace=False, n_samples=len(class_0_indices), random_state=random_state)
 
     balanced_indices = np.concatenate([class_0_indices, class_1_indices])
-    X_balanced = X[balanced_indices]
+    xBalanced = X[balanced_indices]
     y_balanced = y[balanced_indices]
-    print("The length of balanced indcies", len(balanced_indices))
-    print("The length of homing_ids", len(homing_ids))
-    # groups_balanced = homing_ids[balanced_indices]
     groups_balanced = [homing_ids[i] for i in balanced_indices]
-
-    return X_balanced, y_balanced, groups_balanced
+    return xBalanced, y_balanced, groups_balanced
 
 
 def check_there_is_enough_data(count_ans, session_name, cutoff):
@@ -56,14 +147,14 @@ def check_there_is_enough_data(count_ans, session_name, cutoff):
     Args:
         cutoff (int): The minimum number of frames required for each class. 40 frames is 1 second of data"""
     if count_ans[0] < cutoff or count_ans[1] < cutoff:
-        print(f"For session {session_name}, there is less than {cutoff / 40} seconds of data for one class, skipping session")
+        logger.warning(f"For session {session_name}, there is less than {cutoff / 40} seconds of data for one class, skipping session")
         return False
     return True
 
 
 def check_there_are_two_classes(y_train):
     if len(np.unique(y_train)) == 1:
-        print("Skipping fold because there is only one class")
+        logger.warning("Skipping fold because there is only one class")
         return False
     return True
 
@@ -71,22 +162,22 @@ def check_there_are_two_classes(y_train):
 def handle_class_imbalance(classes_extended, session_name, design_matrix, data):
     # Check the class distribution before down sampling
     count_ans = count_class_labels(classes_extended)  # How many frames have 0 or 1
-    print(f"Class distribution before down sampling. 0: {count_ans[0]}, 1: {count_ans[1]}")
+    logger.info(f"Class distribution before down sampling. 0: {count_ans[0]}, 1: {count_ans[1]}")
     if not check_there_is_enough_data(
         count_ans, session_name, cutoff=80
     ):  # If there are less than x seconds of data in either class, skip this session
         return None, None, None  # The cuttoff has not been met
 
     # Down sample the larger class
-    X_balanced, y_balanced, groups_balanced = downsample_larger_class(
+    xBalanced, y_balanced, groups_balanced = downsample_larger_class(
         design_matrix=design_matrix, classes_extended=classes_extended, random_state=1337, homing_ids=data["homing_ids"]
     )
     count_of_downsampled_classes = count_class_labels(y_balanced)
-    print(
+    logger.info(
         f"Class distribution for the entire session after down sampling. 0: {count_of_downsampled_classes[0]}, 1: {count_of_downsampled_classes[1]}"
     )
 
-    return X_balanced, y_balanced, groups_balanced
+    return xBalanced, y_balanced, groups_balanced
 
 
 def are_the_distributions_different(good_data, bad_data):
@@ -253,91 +344,3 @@ def plot_accuracy_across_sessions(data_type, mice_groups, plot_title: str):
     ax.legend(title="Mouse ID")
     plt.tight_layout()
     plt.show()
-
-
-# - Main function for the model -
-
-
-def compute_accuracy_data(data_type, data_type_name, experiments_objects, random_labels=False):
-
-    # Save files
-    save_base = r"Z:\Jasmine_Laurence\single_trial_overview"
-    save_path = os.path.join(save_base, "decoding_spatial_efficiency", data_type_name)
-    dir = make_directory(save_path)
-
-    # Create a dictionary to store the accuracy for each session
-    whole_homing = defaultdict(list)
-    ks_decoding_comparison = defaultdict(defaultdict)
-    coefs = defaultdict(defaultdict)
-
-    # Loop through each session and run logistic regression
-    for (session_name, data), experiment in zip(data_type.items(), experiments_objects):
-        print(f"Running logistic regression for session: {session_name}")
-
-        # Load required data
-        video_df = load_video_data(experiment)
-        design_matrix = data["design_matrix"]  # The X data
-        classes_extended = np.asarray(data["classes_extended"])  # The y data
-        if random_labels:
-            np.random.shuffle(classes_extended)  # randomly shuffle the classes
-
-        X_balanced, y_balanced, groups_balanced = handle_class_imbalance(classes_extended, session_name, design_matrix, data)
-        if X_balanced is None:  # If the cuttoff for the amount of data has not been met
-            continue  # Skip this session
-
-        group_kfold = GroupKFold(n_splits=2)  # k-fold interator variation with non-overlapping groups
-        fold_accuracies = []
-
-        # Assign and remove the last column of the design matrix - Frame numbers are needed to access the behaviour data
-        frame_numbers = X_balanced[:, -1].astype(int)  # Access the last column of the design matrix
-        X_balanced = X_balanced[:, :-1]  # Remove the last column of the design matrix
-
-        for fold, (train_index, test_index) in enumerate(group_kfold.split(X_balanced, y_balanced, groups_balanced)):
-            X_train, X_test = X_balanced[train_index], X_balanced[test_index]
-            y_train, y_test = y_balanced[train_index], y_balanced[test_index]
-            if not check_there_are_two_classes(y_train):
-                break
-            down_count_ans = count_class_labels(y_train)
-            print(f"Class distribution for fold: {fold}. 0: {down_count_ans[0]}, 1: {down_count_ans[1]}")
-            model = LogisticRegression(
-                penalty="l2",
-                dual=False,
-                tol=0.0001,
-                C=1.0,
-                fit_intercept=True,
-                intercept_scaling=1,
-                class_weight="balanced",
-                random_state=1337,
-                solver="lbfgs",
-                max_iter=10000,
-                verbose=0,
-                warm_start=False,
-                n_jobs=None,
-                l1_ratio=None,
-            ).fit(X_train, y_train)
-            y_pred = model.predict(X_test)
-            accuracy = accuracy_score(y_test, y_pred)
-            fold_accuracies.append(accuracy)
-            plot_the_train_test_behavioural_split(video_df, frame_numbers, train_index, test_index, dir, session_name, fold, accuracy)
-            x_ks, y_ks, s_ks, h_ks = plot_class_distributions_and_compute_kolmogorov_Smirnov(
-                frame_numbers, video_df, y_train, dir, session_name, fold, accuracy
-            )
-            ks_decoding_comparison[session_name][fold] = {"x": x_ks, "y": y_ks, "speed": s_ks, "hdir": h_ks, "accuracy": accuracy}
-            coefs[session_name][fold] = model.coef_[0]
-
-        if len(fold_accuracies) == 0:
-            print(f"Skipping session {session_name} because there are no folds to compute accuracy")
-            continue
-
-        # Compute the mean accuracy for the session
-        whole_homing[session_name] = np.mean(fold_accuracies)
-        print(f"Mean accuracy for session {session_name}: {whole_homing[session_name]}")
-
-        # save the data to a dictionary
-        save_data = {"accuracy_data": whole_homing, "ks_data": ks_decoding_comparison, "coefs": coefs}
-
-    # Save the data
-    with open(dir / Path(f"{data_type_name}_accuracy_ks_coeffs_data.pkl"), "wb") as f:
-        pickle.dump(save_data, f)
-
-    return whole_homing, ks_decoding_comparison, coefs
