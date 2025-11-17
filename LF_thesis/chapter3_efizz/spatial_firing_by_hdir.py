11d943be00a3a898e5ace01c7be29ec5a26083e4
LaurenceFreeman <laurence.1.freeman@gmail.com>
2025-11-17 13:37:24 +0000

    bump

--- LF_thesis/chapter3_efizz/spatial_firing_by_hdir.py
+++ LF_thesis/chapter3_efizz/spatial_firing_by_hdir.py
@@ -1,4 +1,6 @@
-"""a set of functions for visualizing the tuning (e.g. spatial) of neurons"""
+"""a set of functions for visualizing the tuning (e.g. spatial) of neurons
+
+cluster 17 in jal004 flipppuf19sept_2023_09_19T14_10_56 coloured by head direction (hdir)"""
 
 # set up
 from loguru import logger
@@ -11,7 +13,7 @@ import polars as pl
 from behave_analysis.analyze.filtering_data.filtering_functions import filter_video_dataframe
 from behave_analysis.visualize.behaviour.behavioral_stats import hsv_hdir_colormap
 
-ANGLE_BIN_EDGES = np.linspace(-180, 180, 361)
+ANGLE_BIN_EDGES = np.linspace(0, 360, 361)
 
 path = r"Z:\Jasmine_Laurence\Experimental_Data\JAL004\004_flipppuf19sept_2023_09_19T14_10_56\processed_data\good_video_spike_count_df.parquet"
 
@@ -25,7 +27,7 @@ print(data.columns)
 
 def digitize_head_direction(hdir_radians):
     """Convert radians to hsv_hdir_colormap bin indices (1-360)."""
-    degrees = np.rad2deg(np.asarray(hdir_radians))
+    degrees = np.mod(np.rad2deg(np.asarray(hdir_radians)), 360)
     bins = np.digitize(degrees, ANGLE_BIN_EDGES, right=False)
     return np.clip(bins, 1, 360)
 
@@ -57,7 +59,9 @@ def spatial_position_firing_hdir(video_df, save_path):
     # video_df = filter_video_dataframe(video_df, condition=None, excl_stationary=False, exclude_escape=True, exclude_homings=False)
 
     ids = video_df["spike_clusters"].unique().to_list()
+    print(ids)
     this = ids[10]
+    print(f"Plotting cluster {this}")
     filt = video_df.filter((video_df["spike_clusters"] == this))
     fig = plt.figure(figsize=(8, 6), dpi=100)
     gs = fig.add_gridspec(1, 2, width_ratios=[4, 1], wspace=0.3)
@@ -68,36 +72,34 @@ def spatial_position_firing_hdir(video_df, save_path):
     arena_radius = 460
 
     # Plot transparent points for all mouse positions in grey
-    #circular mask the video_df to only include points within the arena
-    video_df = video_df.filter(
-        ((video_df["mouse_x_position"] - arena_center_x) ** 2 + (video_df["mouse_y_position"] - arena_center_y) ** 2) <= (arena_radius ** 2)
-    )
-    
-    plot_ax.scatter(video_df["mouse_x_position"].to_numpy(), video_df["mouse_y_position"].to_numpy(), s=7, c="grey", linewidths=0, marker=".", alpha=0.01)
+    # circular mask the video_df to only include points within the arena
+    video_df = video_df.filter(((video_df["mouse_x_position"] - arena_center_x) ** 2 + (video_df["mouse_y_position"] - arena_center_y) ** 2) <= (arena_radius**2))
+
+    plot_ax.scatter(
+        video_df["mouse_x_position"].to_numpy(), video_df["mouse_y_position"].to_numpy(), s=7, c="grey", linewidths=0, marker=".", alpha=0.01, rasterized=True
+    )  # all positions in grey
 
     # Plot firing positions coloured by hdir
     spike_filt = filt.filter(filt["spike_count"] > 0)
 
     # filter spike_filt only out of shelter and no escapes
     spike_filt = filter_video_dataframe(spike_filt, condition=None, excl_stationary=False, exclude_escape=True, exclude_homings=False)
-    
+
     # Remove any points that are outside the arena ciruclar bounds using the center and radius of the arena to filter
-    spike_filt = spike_filt.filter(
-        ((spike_filt["mouse_x_position"] - arena_center_x) ** 2 + (spike_filt["mouse_y_position"] - arena_center_y) ** 2) <= (arena_radius ** 2)
-    )
+    spike_filt = spike_filt.filter(((spike_filt["mouse_x_position"] - arena_center_x) ** 2 + (spike_filt["mouse_y_position"] - arena_center_y) ** 2) <= (arena_radius**2))
 
     hdir = digitize_head_direction(spike_filt["hdir"])
     cc = hsv_hdir_colormap(hdir)
-    plot_ax.scatter(spike_filt["mouse_x_position"].to_numpy(), spike_filt["mouse_y_position"].to_numpy(), s=7, c=cc, linewidths=0, marker=".")  # this neuron's firing coloured by hdir
+    plot_ax.scatter(
+        spike_filt["mouse_x_position"].to_numpy(), spike_filt["mouse_y_position"].to_numpy(), s=7, c=cc, linewidths=0, marker=".", rasterized=True
+    )  # this neuron's firing coloured by hdir
 
     plot_ax.set_axis_off()
     plot_ax.invert_yaxis()
     plot_ax.set_aspect("equal")
 
     generate_color_wheel(wheel_ax)
-
-    plt.show()
-
+    
     plt.savefig(str(save_path) + "_clusters_spatial_firing_hdir_colored_clu11" + ".eps", format="eps", dpi=100)
     # plt.savefig(str(save_path) + "_clusters_spatial_firing_hdir_colored_clu11" + ".png", format="png", dpi=300)
     plt.close()
