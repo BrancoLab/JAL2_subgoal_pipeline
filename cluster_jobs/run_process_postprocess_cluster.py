c4f0537e91a0fff0f1a73b90c32f72943d50e670
Jasmine Reggiani <jasmine.reggiani@gmail.com>
2026-07-28 14:33:09 +0100

    run analyze efixx cluster

--- cluster_jobs/run_process_postprocess_cluster.py
+++ cluster_jobs/run_process_postprocess_cluster.py
@@ -12,15 +12,37 @@ from databank import cluster_experiments_objects
 from behave_analysis.process.process import Process
 from behave_analysis.postprocess.pp_main import Postprocessor
 from settings.settings_process import settings_process
+from run.run_analyze_efizz import analyze_efizz
 
 assert len(cluster_experiments_objects) != 0, "Session list should not be empty"
 
-for session_id in cluster_experiments_objects:
-	logger.info("Creating a session with the following details: {}", session_id)
-	process_object = Process(session_id)
-	process_object.create_session(settings_process)
+# for session_id in cluster_experiments_objects:
+# 	logger.info("Creating a session with the following details: {}", session_id)
+# 	process_object = Process(session_id)
+# 	process_object.create_session(settings_process)
+# logger.success("Processing complete")
+
+# for session_id in cluster_experiments_objects:
+# 	session = Process(session_id).load_session()
+# 	logger.info("Loaded a session with the following details: {}", session_id)
+# 	Postprocessor(session)
+# logger.success("Postprocessing complete")
+
+
+# analyze efizz data
+from behave_analysis.analyze.analyze_efizz import AnalyzeEfizz
+from settings.settings_analyze_efizz import Settings_ae
+
+analysis_name='EscapePattern'
+variable = ['frac_route in homing&escape', 'frac_route in to_subgoal_homing&escape', 'frac_route in correct_full_homing&escape']
 
 for session_id in cluster_experiments_objects:
-	session = Process(session_id).load_session()
-	logger.info("Loaded a session with the following details: {}", session_id)
-	Postprocessor(session)
+    session = Process(session_id).load_session()
+    logger.info("Loaded a session with the following details: {}".format(session_id))
+
+    aefizz = AnalyzeEfizz(session, Settings_ae)
+    aefizz.load_data(analysis_name)
+    for var in variable:
+        aefizz.execute(analysis_name, var)
+
+logger.success("Efizz analysis pipeline complete")
